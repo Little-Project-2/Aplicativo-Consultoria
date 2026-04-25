dc40fcb140913721bb6917bb0d3f33accd6c0ad1
Nicolas Silveira <nicolas9silveira@gmail.com>
2026-04-24 22:16:16 -0300

    Update app-config.example.js

--- app-config.example.js
+++ app-config.example.js
@@ -1,4 +1,4 @@
 window.__APP_CONFIG__ = {
-    supabaseUrl: "https://SEU-PROJETO.supabase.co",
-    supabaseAnonKey: "SUA_CHAVE_ANON_PUBLICA"
+    supabaseUrl: "https://gngzomkjjxshbywygvhr.supabase.co",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImduZ3pvbWtqamp4c2hidXlndmhydCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzYxMjg4MzU1LCJleHAiOjIwNzY4NjQzNTV9.q2jJ_H9l470qB3iS84r4r0926sJ4J4Qj73"
 };
